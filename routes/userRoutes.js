5b536bf2223eb7e11a79f06d67e80cfb6b976a9d
Dhanashree <dhanashreeakharade@gmail.com>
2026-03-29 17:31:10 -0400

    created callback route that GitHub will redirect to after the user approves

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,6 +4,8 @@ const router = express.Router();
 
 import User from '../models/User.js'
 import { signToken } from '../utils/auth.js'
+
+import passport from '../config/passport.js'
  
 // POST /api/users/register - Create a new user
 router.post('/register', async (req, res) => {
@@ -33,5 +35,22 @@ router.post('/login', async (req, res) => {
   const token = signToken(user);
   res.json({ token, user });
 });
+
+router.get(
+  '/auth/github/callback',
+  passport.authenticate('github',{
+    failureRedirect: '/login',            // Where to redirect if user denies
+    session: false                   //// We are using tokens, not sessions
+  })  ,
+  (req,res) => {
+   // At this point, `req.user` is the user profile returned from the verify callback.
+    // We can now issue our own JWT to the user.
+    const token = signToken(req.user);
+
+    // Redirect the user to the frontend with the token, or send it in the response
+    res.redirect(`http://localhost:3000?token=${token}`)
+  }
+);
+
  
 export default router
